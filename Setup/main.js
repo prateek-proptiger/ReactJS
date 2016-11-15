1a407386ab3f9b2be5d0e490ba0705ff41c1d2ff
prateek-proptiger <prateeksehgal.iitbhu@gmail.com>
2016-11-15 15:26:27 +0530

    delete Setup folder

--- Setup/main.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"; // -- Imports App Component after creating it.
-ReactDOM.render(<App />, document.getElementById('app'));
